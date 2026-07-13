b1fc8524a59340712fdbadcf72aadf7835172be8
EmyCodeIs <161089783+EmyCodeIs@users.noreply.github.com>
2026-07-13 15:03:51 -0300

    fix: conferir etiqueta de vendedor antes de cada envio automático

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,6 +2,14 @@
 
 const wppconnect = require('@wppconnect-team/wppconnect');
 const { patchWppConnect } = require('./services/strictHandoffPolicy');
+const { installSellerLabelSendGuard } = require('./services/sellerLabelSendGuard');
 
 patchWppConnect(wppconnect);
+
+const strictCreate = wppconnect.create.bind(wppconnect);
+wppconnect.create = async (...args) => {
+  const client = await strictCreate(...args);
+  return installSellerLabelSendGuard(client);
+};
+
 require('./index');
