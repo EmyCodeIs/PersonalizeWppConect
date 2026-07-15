0e56c5283c285f7e70ff40ac83d0f6d610a0698e
EmyCodeIs <161089783+EmyCodeIs@users.noreply.github.com>
2026-07-15 00:11:49 -0300

    fix: ativar resolução LID e silêncio após conclusão

--- src/start-with-required-labels.js
+++ src/start-with-required-labels.js
@@ -3,12 +3,15 @@
 const serviceLabels = require('./core/serviceLabels');
 const { ensureRequiredLabelsOnce } = require('./core/requiredLabelsStartup');
 const { installIdempotentServiceLabels } = require('./core/idempotentServiceLabels');
+const { installLidServiceLabelFix } = require('./core/lidServiceLabelFix');
 
 serviceLabels.initializeServiceLabels = ensureRequiredLabelsOnce;
 installIdempotentServiceLabels();
+installLidServiceLabelFix();
 
 require('./core/handoffPreload');
 require('./core/resetCleanupPreload');
 require('./core/customerFlowFixPreload');
 require('./core/preferredSellerNotePreload');
+require('./core/completedFlowSilencePreload');
 require('./bootstrap');
